c1265dbfed12027596b15ff30521fddf6e48c8d6
Oskar Bley <oskar@prgramming-php.net>
2013-07-22 00:37:35 +0200

    we need both mapping drivers

--- config/module.config.php
+++ config/module.config.php
@@ -6,10 +6,15 @@ return array(
                 'class' => 'Doctrine\ORM\Mapping\Driver\XmlDriver',
                 'paths' => __DIR__ . '/xml/goaliorememberme'
             ),
+            'goalioremembermedoctrineorm_entity' => array(
+                'class' => 'Doctrine\ORM\Mapping\Driver\XmlDriver',
+                'paths' => __DIR__ . '/xml/goalioremembermedoctrineorm'
+            ),
 
             'orm_default' => array(
                 'drivers' => array(
-                    'GoalioRememberMe\Entity'  => 'goaliorememberme_entity'
+                    'GoalioRememberMe\Entity'  => 'goaliorememberme_entity',
+                    'GoalioRememberMeDoctrineORM\Entity'  => 'goalioremembermedoctrineorm_entity'
                 )
             )
         )
